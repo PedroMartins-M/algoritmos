11fb28d82a2b91db602f686d16b1d6a6b6337e47
Pedro Martins <pedromartinss996@gmail.com>
2025-08-04 20:53:20 -0300

    Criação do código C# para demonstrar textos com Placeholder e valores formatados

--- 02_Variaveis/DecimalFormatado.cs
+++ 02_Variaveis/DecimalFormatado.cs
@@ -5,7 +5,25 @@ class DeclaracaoVariavelDecimalFormatado
 	{
 		decimal x = 0.999m;
 		decimal y = 9999999999999999999999999999m;
+		/*
+		Utilizando Placeholder para demonstrar o conteúdo
+		{0;C}
+		0 = Posiçao de Marcação
+		C = Formação de Moeda (Currency)
+		*/
+		
 		Console.WriteLine("Minha quantia = {0:c}",x);
 		Console.WriteLine("Sua quantia = {0:c}",y);
+		Console.WriteLine("Valor de x = {0: F3} e Valor de y = (1:c)",x,y);
+		
+		/*
+		Interpolação de String, só funciona no .Net versão 6.0 acima
+		*/
+		
+		/*
+		Console.WriteLine($"Minha quantia = {0:c}",x);
+		Console.WriteLine($"Sua quantia = {0:c}",y);
+		Console.WriteLine($"Valor de x = {0: F3} e Valor de y = (1:c)",x,y);
+		*/
 		}
 	}
