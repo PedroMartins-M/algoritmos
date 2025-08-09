0ae93f707e87943b9c70ac1185dd392dee69d189
Pedro Martins <pedromartinss996@gmail.com>
2025-08-08 21:42:58 -0300

    Loop While

---
+++ 05_Loops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_Loops
+{
+    internal class Program
+    {
+        static void Main(string[ ] args)
+        {
+            //criação de um vetor para armazenamento de 100 elementos
+            // string [] nomes = {"Fulano", "Beltrano, "Sicrano"}
+            string[] nomes = new string[100];
+            string continuar = "S";
+
+            //será meu indice para atribuição no setor
+            int contador = 0;
+
+            //Loop while
+            //Sintaxe: while( espressão booleana)
+            //Método ToUpper(): converte o texto para maiúsculo
+            while (continuar.ToUpper() == "S")
+            {
+                Console.WriteLine("Digite o {0}ª nome: ", contador+1);
+                //Append: adcionado um item no vetor
+                nomes[contador] = Console.ReadLine();
+
+                //incrementar o contador
+
+                contador++;
+                Console.WriteLine("Deseja continuar? (S/N)");
+                continuar = Console.ReadLine();
+            }
+            Console.WriteLine("Nomes informados: ");
+            foreach (string str in nomes)
+            {
+                if (str != null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
