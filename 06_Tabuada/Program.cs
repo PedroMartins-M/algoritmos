c4c9661026ab904475ae6af33537a1fbc1c70d3d
Pedro Martins <pedromartinss996@gmail.com>
2025-08-08 22:15:16 -0300

    Tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para ver a tabuada: ");
+
+            // Lê o número digitado e converte para inteiro
+            if (int.TryParse(Console.ReadLine(), out int numero))
+            {
+                Console.WriteLine($"\nTabuada do {numero}:\n");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{numero} x {i} = {numero * i}");
+                }
+            }
+        }
+    }
+}
