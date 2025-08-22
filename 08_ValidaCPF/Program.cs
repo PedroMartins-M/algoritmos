df465254675e9c48ed1bcbaac292e193b33bcf4f
Pedro Martins <pedromartinss996@gmail.com>
2025-08-21 22:18:48 -0300

    Código Validação de CPF

---
+++ 08_ValidaCPF/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _08_ValidaCPF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o CPF: ");
+            string cpf = Console.ReadLine();
+
+            if (ValidarCPF(cpf))
+                Console.WriteLine("CPF válido!");
+            else
+                Console.WriteLine("CPF inválido!");
+        }
+
+        static bool ValidarCPF(string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf))
+                return false;
+
+            Regex regex = new Regex(@"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$");
+
+            if (!regex.IsMatch(cpf))
+                return false;
+
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+            if (cpf.All(c => c == cpf[0])) 
+                return false;
+
+            int[] multiplicadores1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicadores2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += (tempCpf[i] - '0') * multiplicadores1[i];
+
+            int resto = soma % 11;
+            int digito1 = resto < 2 ? 0 : 11 - resto;
+
+            tempCpf += digito1;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += (tempCpf[i] - '0') * multiplicadores2[i];
+
+            resto = soma % 11;
+            int digito2 = resto < 2 ? 0 : 11 - resto;
+
+            return cpf.EndsWith(digito1.ToString() + digito2.ToString());
+        }
+    }
+}
